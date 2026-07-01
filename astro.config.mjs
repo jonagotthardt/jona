d73d8e576e74c5b9bb1f7a0bd2c370b3e9b1cec3
Jona Gotthardt <jona.gotthardt@icloud.com>
2026-07-01 21:56:33 +0200

    Configure for GitHub Pages: static output, remove Cloudflare, add deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,9 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://jonagotthardt.de",
   integrations: [mdx(), sitemap()],
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true,
-    },
-  }),
+  output: "static",
 });
